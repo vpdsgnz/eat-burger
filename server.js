3692e28f051f4b6d979c0aac16ee6be572de81de
vpdsgnz <vpdsgnz@hotmail.com>
2019-06-25 15:18:39 -0400

    config, controllers, modelsand views

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("app/public"));
+
+require("./app/routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
